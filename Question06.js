143026617526b9d767f019bc9b921ad30b9bda76
Thavanesan Thabiseak <thabuthavanesan@gmail.com>
2024-06-07 12:30:33 +0530

    Question06

---
+++ Question06.js
@@ -0,0 +1,8 @@
+function reverseArray(arr) {
+    return arr.slice().reverse();
+  }
+  
+  const originalArray = [1, 2, 3, 4, 5];
+  const reversedArray = reverseArray(originalArray);
+  console.log(reversedArray);
+  
